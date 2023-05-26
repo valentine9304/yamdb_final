26796342f5718b22bcd8684dfbcb5df0789dc4d5
Valentine Saint <valentine9304@gmail.com>
2023-05-26 19:52:19 +0700

    dsad

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="secret")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
